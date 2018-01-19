1befbe47e377b29453ef5af7ae963b040d6bbd9c
quydx <quy196hp8@gmail.com>
2018-01-19 11:16:28 +0700

    update

--- test.py
+++ test.py
@@ -4,9 +4,10 @@ from pprint import pprint
 
 es = Elasticsearch(['localhost:9200'])
 
-es.indices.create(index='test-index', ignore=400)
-health = es.cluster.health(wait_for_status='yellow', request_timeout=1)
-pprint(es.search(index='test-index', filter_path=['hits.hits._id', 'hits.hits._type']))
-pprint(health)
+res = es.search(index="suricata-bd-alert-*", body={"query": {"match_all": {}}} , filter_path=['hits.hits._index'])
+print("Got %d Hits:" % res['hits']['total'])
+# for hit in res['hits']['hits']:
+#     print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
+
 
 
